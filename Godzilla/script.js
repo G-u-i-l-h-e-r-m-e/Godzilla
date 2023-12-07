cd69b87067030be7048e2e4e8a25cc090a9d8a20
G-u-i-l-h-e-r-m-e <142812776+G-u-i-l-h-e-r-m-e@users.noreply.github.com>
2023-12-06 21:03:34 -0300

    Add files via upload
    site adicionado ao git

---
+++ Godzilla/script.js
@@ -0,0 +1,307 @@
+
+function aparecerCard1(){
+    var clickItem1 = document.getElementById("item1");
+    var clickItem2 = document.getElementById("item2");
+    var clickItem3 = document.getElementById("item3");
+    var clickItem4 = document.getElementById("item4");
+    var clickItem5 = document.getElementById("item5");
+    var clickItem6 = document.getElementById("item6");
+    var aparecer = document.getElementById("aparecer");
+      
+      aparecer.style.display = "none";
+      clickItem1.style.display = "inline";
+      clickItem2.style.display = "none";
+      clickItem3.style.display = "none";
+      clickItem4.style.display = "none";
+      clickItem5.style.display = "none";
+      clickItem6.style.display = "none";
+
+      var cea = document.getElementById("ImgCE");
+        var cda = document.getElementById("imgCD");
+        var mea = document.getElementById("imgME");
+        var mda = document.getElementById("imgMD");
+        var bea = document.getElementById("imgBE");
+        var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CEA.png');
+      cda.setAttribute('src', 'img/CD.png');
+      mea.setAttribute('src', 'img/ME.png');
+      mda.setAttribute('src', 'img/MD.png');
+      bea.setAttribute('src', 'img/BE.png');
+      bda.setAttribute('src', 'img/BD.png');
+      
+    }
+    function aparecerCard2(){
+  
+        var clickItem1 = document.getElementById("item1");
+        var clickItem2 = document.getElementById("item2");
+        var clickItem3 = document.getElementById("item3");
+        var clickItem4 = document.getElementById("item4");
+        var clickItem5 = document.getElementById("item5");
+        var clickItem6 = document.getElementById("item6");
+        var aparecer = document.getElementById("aparecer");
+
+      aparecer.style.display = "inline";
+      clickItem1.style.display = "none";
+      clickItem2.style.display = "inline";
+      clickItem3.style.display = "none";
+      clickItem4.style.display = "none";
+      clickItem5.style.display = "none";
+      clickItem6.style.display = "none";
+
+        var cea = document.getElementById("ImgCE");
+        var cda = document.getElementById("imgCD");
+        var mea = document.getElementById("imgME");
+        var mda = document.getElementById("imgMD");
+        var bea = document.getElementById("imgBE");
+        var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CE.png');
+      cda.setAttribute('src', 'img/CDA.png');
+      mea.setAttribute('src', 'img/ME.png');
+      mda.setAttribute('src', 'img/MD.png');
+      bea.setAttribute('src', 'img/BE.png');
+      bda.setAttribute('src', 'img/BD.png');
+  
+    }
+    function aparecerCard3(){
+
+      var clickItem1 = document.getElementById("item1");
+      var clickItem2 = document.getElementById("item2");
+      var clickItem3 = document.getElementById("item3");
+      var clickItem4 = document.getElementById("item4");
+      var clickItem5 = document.getElementById("item5");
+      var clickItem6 = document.getElementById("item6");
+      var aparecer = document.getElementById("aparecer");
+      
+      aparecer.style.display = "inline";
+      clickItem1.style.display = "none";
+      clickItem2.style.display = "none";
+      clickItem3.style.display = "inline";
+      clickItem4.style.display = "none";
+      clickItem5.style.display = "none";
+      clickItem6.style.display = "none";
+  
+    var cea = document.getElementById("ImgCE");
+        var cda = document.getElementById("imgCD");
+        var mea = document.getElementById("imgME");
+        var mda = document.getElementById("imgMD");
+        var bea = document.getElementById("imgBE");
+        var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CE.png');
+      cda.setAttribute('src', 'img/CD.png');
+      mea.setAttribute('src', 'img/MEA.png');
+      mda.setAttribute('src', 'img/MD.png');
+      bea.setAttribute('src', 'img/BE.png');
+      bda.setAttribute('src', 'img/BD.png');
+  }
+  
+    function aparecerCard4(){
+
+        var clickItem1 = document.getElementById("item1");
+        var clickItem2 = document.getElementById("item2");
+        var clickItem3 = document.getElementById("item3");
+        var clickItem4 = document.getElementById("item4");
+        var clickItem5 = document.getElementById("item5");
+        var clickItem6 = document.getElementById("item6");
+        var aparecer = document.getElementById("aparecer");
+
+      aparecer.style.display = "inline";
+      clickItem1.style.display = "none";
+      clickItem2.style.display = "none";
+      clickItem3.style.display = "none";
+      clickItem4.style.display = "inline";
+      clickItem5.style.display = "none";
+      clickItem6.style.display = "none";
+
+        var cea = document.getElementById("ImgCE");
+        var cda = document.getElementById("imgCD");
+        var mea = document.getElementById("imgME");
+        var mda = document.getElementById("imgMD");
+        var bea = document.getElementById("imgBE");
+        var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CE.png');
+      cda.setAttribute('src', 'img/CD.png');
+      mea.setAttribute('src', 'img/ME.png');
+      mda.setAttribute('src', 'img/MDA.png');
+      bea.setAttribute('src', 'img/BE.png');
+      bda.setAttribute('src', 'img/BD.png');
+    }
+  
+    function aparecerCard5(){
+
+      var clickItem1 = document.getElementById("item1");
+      var clickItem2 = document.getElementById("item2");
+      var clickItem3 = document.getElementById("item3");
+      var clickItem4 = document.getElementById("item4");
+      var clickItem5 = document.getElementById("item5");
+      var clickItem6 = document.getElementById("item6");
+      var aparecer = document.getElementById("aparecer");
+      
+      aparecer.style.display = "inline";
+      clickItem1.style.display = "none";
+      clickItem2.style.display = "none";
+      clickItem3.style.display = "none";
+      clickItem4.style.display = "none";
+      clickItem5.style.display = "inline";
+      clickItem6.style.display = "none";
+
+    var cea = document.getElementById("ImgCE");
+    var cda = document.getElementById("imgCD");
+    var mea = document.getElementById("imgME");
+    var mda = document.getElementById("imgMD");
+    var bea = document.getElementById("imgBE");
+    var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CE.png');
+      cda.setAttribute('src', 'img/CD.png');
+      mea.setAttribute('src', 'img/ME.png');
+      mda.setAttribute('src', 'img/MD.png');
+      bea.setAttribute('src', 'img/BEA.png');
+      bda.setAttribute('src', 'img/BD.png');
+  }
+  
+  function aparecerCard6(){
+    var clickItem1 = document.getElementById("item1");
+    var clickItem2 = document.getElementById("item2");
+    var clickItem3 = document.getElementById("item3");
+    var clickItem4 = document.getElementById("item4");
+    var clickItem5 = document.getElementById("item5");
+    var clickItem6 = document.getElementById("item6");
+    var aparecer = document.getElementById("aparecer");
+      
+    aparecer.style.display = "inline"
+    clickItem1.style.display = "none";
+    clickItem2.style.display = "none";
+    clickItem3.style.display = "none";
+    clickItem4.style.display = "none";
+    clickItem5.style.display = "none";
+    clickItem6.style.display = "inline";
+
+    var cea = document.getElementById("ImgCE");
+    var cda = document.getElementById("imgCD");
+    var mea = document.getElementById("imgME");
+    var mda = document.getElementById("imgMD");
+    var bea = document.getElementById("imgBE");
+    var bda = document.getElementById("imgBD");
+      
+      cea.setAttribute('src', 'img/CE.png');
+      cda.setAttribute('src', 'img/CD.png');
+      mea.setAttribute('src', 'img/ME.png');
+      mda.setAttribute('src', 'img/MD.png');
+      bea.setAttribute('src', 'img/BE.png');
+      bda.setAttribute('src', 'img/BDA.png');
+  }
+  
+  function AddTextoRodaIgreja(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.style.width = "50%";
+    imgRoda.setAttribute('src', 'img/igreja.png');
+  }
+  function RemoveTextoRodaIgreja(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "CLIQUE E VEJA COMO FUNCIONA O PONTO DE COLETA";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.setAttribute('src', '');
+    imgRoda.style.width = "0%";
+  }
+
+  function AddTextoRodaCondominio(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.style.width = "40%";
+    imgRoda.style.marginRight  = "50px";
+    imgRoda.style.marginTop  = "50px";
+    imgRoda.setAttribute('src', 'img/condominio.png');
+  }
+
+  function RemoveTextoRodaCondominio(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "CLIQUE E VEJA COMO FUNCIONA O PONTO DE COLETA";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.setAttribute('src', '');
+    imgRoda.style.width = "0%";
+    imgRoda.style.marginRight  = "0px";
+    imgRoda.setAttribute('src', '');
+  }
+
+  function AddTextoRodaProjetos(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.style.width = "40%";
+    imgRoda.style.marginRight  = "70px";
+    imgRoda.style.marginTop  = "20px";
+    imgRoda.setAttribute('src', 'img/projetoSocial.png');
+  }
+
+  function RemoveTextoRodaProjetos(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "CLIQUE E VEJA COMO FUNCIONA O PONTO DE COLETA";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.setAttribute('src', '');
+    imgRoda.style.width = "0%";
+    imgRoda.style.marginRight  = "0px";
+    imgRoda.style.marginTop  = "0px";
+    imgRoda.setAttribute('src', '');
+  }
+
+
+  function AddTextoRodaMercado(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.style.width = "40%";
+    imgRoda.style.marginRight  = "40px";
+    imgRoda.style.marginTop  = "80px";
+    imgRoda.setAttribute('src', 'img/mercado.png');
+  }
+
+  function RemoveTextoRodaMercado(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "CLIQUE E VEJA COMO FUNCIONA O PONTO DE COLETA";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.setAttribute('src', '');
+    imgRoda.style.width = "0%";
+    imgRoda.style.marginRight  = "0px";
+    imgRoda.style.marginTop  = "0px";
+    imgRoda.setAttribute('src', '');
+  }
+
+
+  function AddTextoRodaEstabelecimento(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.style.width = "40%";
+    imgRoda.style.marginRight  = "30px";
+    imgRoda.style.marginTop  = "80px";
+    imgRoda.setAttribute('src', 'img/estabelecimento.png');
+  }
+
+  function RemoveTextoRodaEstabelecimento(){
+    txt = document.getElementById("textoRoda");
+    txt.innerHTML = "CLIQUE E VEJA COMO FUNCIONA O PONTO DE COLETA";
+
+    imgRoda = document.getElementById("imgRodaIgreja");
+    imgRoda.setAttribute('src', '');
+    imgRoda.style.width = "0%";
+    imgRoda.style.marginRight  = "0px";
+    imgRoda.style.marginTop  = "0px";
+    imgRoda.setAttribute('src', '');
+  }
+
